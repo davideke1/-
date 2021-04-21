19b26d4610fc0afc8f52ce0c9c8a3102c6714aed
Israel Abraham <israelvictory87@gmail.com>
2021-04-21 08:02:41 +0100

    Created home and article view

--- crud_blog/views.py
+++ crud_blog/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "crud_blog/index.html")
+
+
+def article(request):
+    return render(request, "crud_blog/blog-single.html")
