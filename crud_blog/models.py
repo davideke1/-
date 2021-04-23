c48c82697c66f6e97c3f8deca8ec9f5cf0aa1903
Israel Abraham <israelvictory87@gmail.com>
2021-04-23 17:34:14 +0100

    Update models.py

--- crud_blog/models.py
+++ crud_blog/models.py
@@ -7,6 +7,7 @@ class Article(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=150)
     content = models.TextField()
+    image = models.ImageField(upload_to="images/", blank=True, null=True)
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=False)
 
     def __str__(self):
@@ -14,6 +15,12 @@ class Article(models.Model):
 
     def get_absolute_url(self):
         return reverse("blog:article", kwargs={"pk": self.pk})
+
+    def get_update_url(self):
+        return reverse("blog:update", kwargs={"pk": self.pk})
+
+    def get_delete_url(self):
+        return reverse("blog:delete", kwargs={"pk": self.pk})
     
 
 
