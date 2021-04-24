c35f7a1a5b348cf7a08d75e2751602a18819a331
Israel Abraham <israelvictory87@gmail.com>
2021-04-24 07:18:05 +0100

    Update settings.py

--- zuri/settings.py
+++ zuri/settings.py
@@ -30,7 +30,7 @@ with open(os.path.join(BASE_DIR, 'config.json')) as config_file:
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", "zuri-crud-blog.herokuapp.com"]
 
