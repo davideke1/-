26b11fdb930bfa58aea5e1bcea79492f0aa58897
Israel Abraham <israelvictory87@gmail.com>
2021-04-24 07:34:32 +0100

    Update settings.py

--- zuri/settings.py
+++ zuri/settings.py
@@ -20,17 +20,13 @@ load_dotenv(find_dotenv())
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-with open(os.path.join(BASE_DIR, 'config.json')) as config_file:
-    config = json.load(config_file)
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", "zuri-crud-blog.herokuapp.com"]
 
@@ -100,7 +96,7 @@ elif DEBUG is False:
 
             'USER': os.environ.get("USER"),
 
-            'PASSWORD': os.environ.get("PASSWORd"),
+            'PASSWORD': os.environ.get("PASSWORD"),
 
             'HOST': os.environ.get("HOST"),
 
